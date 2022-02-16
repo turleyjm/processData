8a8e61b59fdf424ca467020d9105b62b409883d1
Jake Turley <turleyjm@gmail.com>
2022-02-16 16:57:47 +0000

    minor edits

--- adjustStartOfVideo.py
+++ adjustStartOfVideo.py
@@ -9,42 +9,42 @@ import tifffile
 
 f = open("pythonText.txt", "r")
 
-filename = f.read()
+filename = "WoundS18h22"
 stackFile = f"datProcessing/{filename}/{filename}.tif"
 stack = sm.io.imread(stackFile).astype(int)
 T, Z, C, Y, X = stack.shape
 
 # Negative
-shift_x = 15
-shift_y = 30
-t = 0
+shift_x = 25
+shift_y = 25
+t = 2  # number of time frames need switching
 
 if shift_x < 0:
     if shift_y < 0:
-        stack[t, :, :, 0 : Y + shift_y, 0 : X + shift_x] = stack[
-            t, :, :, -shift_y:Y, -shift_x:X
+        stack[:t, :, :, 0 : Y + shift_y, 0 : X + shift_x] = stack[
+            :t, :, :, -shift_y:Y, -shift_x:X
         ]
-        stack[t, :, :, 0:Y, X + shift_x : X] = 0
-        stack[t, :, :, Y + shift_y : Y, 0:X] = 0
+        stack[:t, :, :, 0:Y, X + shift_x : X] = 0
+        stack[:t, :, :, Y + shift_y : Y, 0:X] = 0
     else:
-        stack[t, :, :, shift_y:Y, 0 : X + shift_x] = stack[
-            t, :, :, 0 : Y - shift_y, -shift_x:X
+        stack[:t, :, :, shift_y:Y, 0 : X + shift_x] = stack[
+            :t, :, :, 0 : Y - shift_y, -shift_x:X
         ]
-        stack[t, :, :, 0:Y, X + shift_x : X] = 0
-        stack[t, :, :, 0:shift_y, 0:X] = 0
+        stack[:t, :, :, 0:Y, X + shift_x : X] = 0
+        stack[:t, :, :, 0:shift_y, 0:X] = 0
 else:
     if shift_y < 0:
-        stack[t, :, :, 0 : Y + shift_y, shift_x:X] = stack[
-            t, :, :, -shift_y:Y, 0 : X - shift_x
+        stack[:t, :, :, 0 : Y + shift_y, shift_x:X] = stack[
+            :t, :, :, -shift_y:Y, 0 : X - shift_x
         ]
-        stack[t, :, :, 0:Y, 0:shift_x] = 0
-        stack[t, :, :, Y + shift_y : Y, 0:X] = 0
+        stack[:t, :, :, 0:Y, 0:shift_x] = 0
+        stack[:t, :, :, Y + shift_y : Y, 0:X] = 0
     else:
-        stack[t, :, :, shift_y:Y, shift_x:X] = stack[
-            t, :, :, 0 : Y - shift_y, 0 : X - shift_x
+        stack[:t, :, :, shift_y:Y, shift_x:X] = stack[
+            :t, :, :, 0 : Y - shift_y, 0 : X - shift_x
         ]
-        stack[t, :, :, 0:Y, 0:shift_x] = 0
-        stack[t, :, :, 0:shift_y, 0:X] = 0
+        stack[:t, :, :, 0:Y, 0:shift_x] = 0
+        stack[:t, :, :, 0:shift_y, 0:X] = 0
 
 stack = np.asarray(stack, "uint8")
 tifffile.imwrite(f"datProcessing/{filename}/shift{filename}.tif", stack, imagej=True)
